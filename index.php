b9f46edad0e03dc4a9ddce6e44c93b0a56bafb1b
terrence628 <terrence_lam@hotmail.com>
2015-09-11 22:21:33 -0700

    Added 3 students to index.php Added sort to array

--- index.php
+++ index.php
@@ -11,7 +11,53 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Hello out there ... I live";
-        ?>
+            
+            include('student.php');
+            $first = new Student();
+
+            //add first student
+            $students = array();
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('home','john@doe.com');
+            $first->add_email('work','jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
+
+            //add second student
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home','albert@braniacs.com');
+            $second->add_email('work1','a_einstein@bcit.ca');
+            $second->add_email('work2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
+            
+            //add third student (myself)
+            $third = new Student();
+            $third->surname = "Lam";
+            $third->first_name = "Terrence";
+            $third->add_email('home','terrence_lam@hotmail.com');
+            $third->add_email('school','tlam88@my.bcit.ca');
+            $third->add_grade(95);
+            $third->add_grade(80);
+            $students['t628'] = $third;
+
+            //sort students
+            ksort($students); 
+            
+            //output the students 
+            foreach($students as $student){
+                echo $student->toString();
+                
+            }
+            
+            
+        ?> 
     </body>
 </html>
